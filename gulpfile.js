ea742e219485494d3a02bacaa70a459e6fa80259
Tyler Bruffy <TyBruffy@gmail.com>
2014-03-21 17:28:42 -0400

    removed need for gulp-less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var fs   = require('fs');
 
 gulp.task('less', function(cb) {
 	console.log( "compiling less files" )
@@ -12,6 +13,11 @@ gulp.task('less', function(cb) {
 	cb();
 });
 
+gulp.task('alt-less', function() {
+	var child  = require('child_process').spawn
+	,	less = child('lessc', ['--yui-compress', './assets/less/docs.less', './assets/css/test.css']);
+})
+
 gulp.task('jekyll', ['less'], function() {
 	console.log( "starting jekyll" )
 	var child  = require('child_process').spawn
@@ -23,4 +29,4 @@ gulp.task('watch', function() {
 	gulp.watch('./assets/less/*.less', ['jekyll']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['alt-less']);
