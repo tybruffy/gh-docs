f3178da7f3d8a43f6926c551cc9841389cf59091
Tyler Bruffy <tybruffy@gmail.com>
2014-03-24 22:47:59 -0400

    improved gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,45 @@
-var gulp = require('gulp');
-var fs   = require('fs');
+var gulp       = require('gulp')
+,	rename     = require("gulp-rename")
+,	less       = require("gulp-less")
+,	jekyll     = require("gulp-jekyll")
+,	gutil      = require("gulp-util")
+,	livereload = require("gulp-livereload")
+,	connect    = require('gulp-connect')
 
 gulp.task('less', function() {
-	var child  = require('child_process').spawn
-	,	less = child('lessc', ['--yui-compress', './assets/less/docs.less', './assets/css/docs.min.css']);
-})
+	gutil.log('compiling less');
+	var child = require('child_process').spawn
+	,	less  = child('lessc', ['--yui-compress', './assets/less/docs.less', './assets/css/docs.min.css']);
+});
 
 gulp.task('jekyll', ['less'], function() {
-	console.log( "starting jekyll" )
+	gutil.log('building jekyll');
 	var child  = require('child_process').spawn
-	,	jekyll = child('jekyll', ['serve']);
+	,	jekyll = child('jekyll', ['build']);
 });
+
+gulp.task('serve', connect.server({
+	root: ['_site'],
+	port: 4000,
+}));
 
 gulp.task('watch', function() {
-	console.log( "watching less files." )
-	gulp.watch('./assets/less/*.less', ['jekyll']);
+	gulp.watch('./assets/**/*.less', ['less']);
+	gulp.watch(['./assets/**/*.*', './**/*.html', '!./_site/**', '!./assets/**/*.less'], ['jekyll']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('develop', ['less', 'jekyll', 'watch', 'serve']);
+
+gulp.task('default', ['develop']);
+
+// gulp.task('less', function() {
+// 	gulp.src('./assets/less/docs.less')
+// 		.pipe(less({
+// 			yui-compress: true,
+// 		}))
+// 		.pipe(rename(function (path) {
+// 			path.basename += ".min";
+// 			path.extname = ".css"
+// 		}))
+// 		.pipe(gulp.dest('./assets/css'))
+// });
