9e3f6f6b543202a41b70d91c25dde35445c64e47
Tyler Bruffy <tybruffy@gmail.com>
2014-03-30 22:43:45 -0400

    messy but working, not always very gulpy

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
-var gulp       = require('gulp')
-,	rename     = require("gulp-rename")
-,	less       = require("gulp-less")
-,	jekyll     = require("gulp-jekyll")
-,	gutil      = require("gulp-util")
-,	livereload = require("gulp-livereload")
-,	connect    = require('gulp-connect')
-,	request    = require('request')
-,	fs         = require('fs')
-,	marked     = require('marked');
+var gulp        = require('gulp')
+,	rename      = require("gulp-rename")
+,	less        = require("gulp-less")
+,	jekyll      = require("gulp-jekyll")
+,	gutil       = require("gulp-util")
+,	livereload  = require("gulp-livereload")
+,	connect     = require('gulp-connect')
+,	request     = require('request')
+,	fs          = require('fs')
+,	marked      = require('marked')
+,	yaml        = require('js-yaml')
+,	frontMatter = require('gulp-front-matter')
+,	fm          = require('front-matter')
+,	es          = require('event-stream')
+,	streamifier = require('streamifier')
+
 
 gulp.task('less', function() {
 	gutil.log('compiling less');
@@ -36,18 +42,22 @@ gulp.task('develop', ['less', 'jekyll', 'watch', 'serve']);
 gulp.task('default', ['develop']);
 
 
-gulp.task('readme', function() {
-	request('https://raw.githubusercontent.com/tybruffy/slide-load/master/README.md', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
 
-			var content = marked(body, {renderer: renderer})
-			count = content.match(/<h1.*id="(.*)">(.*)<\/h1>/g);
-			console.log( count )
+
+gulp.task('readme', function() {
+	var config = yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf8'));
+
+	request(config.project.readme, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var content  = marked(body, {renderer: renderer})
+			convertReadme(content);
 
 			fs.writeFile("./_includes/readme.html", content, function(err) {
-				if (err) {
-					gutil.log(err)
-				}
+				if (err) throw err;
+				gutil.log(
+					gutil.colors.magenta("./_includes/readme.html"), 
+					"created from",
+					gutil.colors.magenta(config.project.readme))
 			})
 
 		}
@@ -55,9 +65,50 @@ gulp.task('readme', function() {
 });
 
 
+function convertReadme(content) {
+	var	regex    = /<h[1-6].*id="(.*)">(.*)<\/h[1-6]>/g
+	,	sections = []
+
+	while ((matches = regex.exec(content)) !== null) {
+		sections.push({ slug: matches[1], name: matches[2] })
+	}
+
+	gulp.src("./index.html")
+		.pipe(rename("index.bak.html"))
+		.pipe(gulp.dest("./"))
+		.pipe(frontMatter({
+			remove: true
+		}))
+		.pipe(addData(sections))
+		.pipe(addFrontMatter())
+		.pipe(rename("index.html"))
+		.pipe(gulp.dest("./"))
+}
+
+addData = function(data) {
+	return es.map(function (file, callback) {
+		file.frontMatter["sections"] = data;
+		callback(null, file)
+	})	
+};
+
+addFrontMatter = function(data) {
+	return es.map(function (file, callback) {
+		var front = yaml.safeDump(file.frontMatter)
+		,	text  = file._contents.toString()
+
+		file._contents = new Buffer("---\n" + front + "---\n" + text)
+		callback(null, file)
+	})	
+};
+
 
 var renderer = new marked.Renderer();
 renderer.heading = function (txt, lvl) {
 	var escTxt = txt.toLowerCase().replace(/[^\w]+/g, '-');
-	return '<h'+ lvl +' class="page-header" id="'+ escTxt +'">'+ txt +'</h'+ lvl +'>';
+	return '<h'+ lvl +' class="page-header" id="'+ escTxt +'">'+ txt +'</h'+ lvl +'>\n';
+}
+
+renderer.code = function (code, lang) {
+	return "{% highlight " + lang + " %}\n"	+ code + "\n{% endhighlight %}\n";
 }
