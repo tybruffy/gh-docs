b707a39220c39c5145bccdafd46bae1187993de9
Tyler Bruffy <tybruffy@gmail.com>
2014-03-31 22:51:01 -0400

    streamified that last commit, needs cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -47,47 +47,25 @@ gulp.task('develop', ['less', 'jekyll', 'watch', 'serve']);
 gulp.task('default', ['develop']);
 
 
-
-
-gulp.task('readme-file', function() {
-	var config = yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf8'));
-
-	request(config.project.readme, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var content  = marked(body, {renderer: renderer})
-			convertReadme(content);
-
-			fs.writeFile("./_includes/readme.html", content, function(err) {
-				if (err) throw err;
-				gutil.log(
-					gutil.colors.magenta("./_includes/readme.html"), 
-					"created from",
-					gutil.colors.magenta(config.project.readme))
-			})
-
-		}
-	})
-});
-
 gulp.task('readme', function() {
 	var config = yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf8'));
-	 var filter = gulpFilter('index.html');
-
+	var filter = gulpFilter('index.html');
 
 	download(config.project.readme)
 		.pipe(convertMD())
 		.pipe(rename("readme.html"))
 		.pipe(gulp.dest("./_includes"))
 		.pipe(parseReadme())
-		.pipe()
-
-		.pipe(logThing())
-		// .pipe(frontMatter({
-		// 	remove: true
-		// }))
-		// .pipe(logThing())
-
-
+		.pipe(fileSwitch("./index.html"))
+		.pipe(rename("index.bak.html"))
+		.pipe(gulp.dest("./"))		
+		.pipe(frontMatter({
+			remove: true
+		}))
+		.pipe(addData("sections"))
+		.pipe(addFrontMatter())
+		.pipe(rename("index.html"))
+		.pipe(gulp.dest("./"))
 });
 
 convertMD = function() {
@@ -102,59 +80,32 @@ convertMD = function() {
 	})	
 };
 
-logThing = function() {
+fileSwitch = function(newFile) {
 	return es.map(function (file, callback) {
-		console.log( file.sections )
+		
+		var text = fs.readFileSync(newFile, {encoding: "utf-8"})
+		file.contents = new Buffer(text)
+
 		callback(null, file)
 	})	
 };
-
-changeThing = function() {
-	return es.map(function (file, callback) {
-		file = file.sections
-		callback(null, file)
-	})	
-};
-
-
 
 parseReadme = function(sections) {
 	return es.map(function (file, callback) {
-		var	regex    = /<h[1-6].*id="(.*)">(.*)<\/h[1-6]>/g
-		,	sections = []
+		var	regex     = /<h[1-6].*id="(.*)">(.*)<\/h[1-6]>/g
+		file.sections = []
 
 		while ((matches = regex.exec(file.contents)) !== null) {
-			sections.push({ slug: matches[1], name: matches[2] })
+			file.sections.push({ slug: matches[1], name: matches[2] })
 		}
-		file.sections = sections
+
 		callback(null, file)
-	})		
+	})
 };
-
-
-function convertReadme(content) {
-	var	regex    = /<h[1-6].*id="(.*)">(.*)<\/h[1-6]>/g
-	,	sections = []
-
-	while ((matches = regex.exec(file.contents)) !== null) {
-		sections.push({ slug: matches[1], name: matches[2] })
-	}
-
-	gulp.src("./index.html")
-		.pipe(rename("index.bak.html"))
-		.pipe(gulp.dest("./"))
-		.pipe(frontMatter({
-			remove: true
-		}))
-		.pipe(addData("sections", sections))
-		.pipe(addFrontMatter())
-		.pipe(rename("index.html"))
-		.pipe(gulp.dest("./"))
-}
 
 addData = function(key, value) {
 	return es.map(function (file, callback) {
-		file.frontMatter[key] = value;
+		file.frontMatter[key] = file.sections;
 		callback(null, file)
 	})	
 };
