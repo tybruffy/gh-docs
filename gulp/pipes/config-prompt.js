ca40cd76212b3c7160845ba6ba033fe422105304
Tyler Bruffy <TyBruffy@gmail.com>
2014-04-10 17:24:12 -0400

    progress made, not modular enough

---
+++ gulp/pipes/config-prompt.js
@@ -0,0 +1,102 @@
+var	gulp     = require('gulp')
+,	es       = require('event-stream')
+,	inquirer = require('inquirer')
+,	yaml     = require('js-yaml')
+,	_        = require('lodash')
+,	fs       = require('fs')
+
+module.exports = function() {
+	return es.map(function(file, cb){
+		inquirer.prompt(questions, function(answers){
+
+			var config  = yaml.safeLoad( fs.readFileSync('./_config.yml', 'utf8') )
+			,	answers = parseAnswers(answers)
+			,	answers = getAnsObj(answers)
+			,	content = yaml.safeDump( _.merge( config, answers ) );
+
+			console.log( new Buffer(content) )
+			// file._contents = new Buffer(content)
+
+			cb(null, file);
+		});
+	});
+};
+
+function getAnsObj( response ) {
+	var projectKeys = ['project', 'repo', 'readme', 'version', 'description']
+	,	authorKeys  = ['author', 'website']
+	,	answers     = {};
+
+	for (prop in response) {
+		var section       = _.indexOf(projectKeys, prop) != -1 ? "project" : "author"
+		,	 key          = prop === section ? "name" : prop;
+
+		answers[section]      = answers[section] || {}
+		answers[section][key] = response[prop]
+	}
+
+	return answers;
+}
+
+function parseAnswers( answers ) {
+	for (prop in answers) {
+		if (answers[prop] === '') {
+			delete answers[prop];
+		} else {
+			answers[prop] = filterProp( prop, answers )
+		}
+	}
+
+	return answers;
+}
+
+function filterProp( prop, answers ) {
+	switch (prop) {
+		case 'readme':
+			if (answers.readme == 'default' && answers.repo) {
+				return 'https://raw.githubusercontent.com' + answers.repo + 'master/README.md'
+			}
+		break;
+		default:
+			return answers[prop];
+		break;
+	}
+}
+
+var questions = [
+	{
+		type: 'input',
+		name: 'project',
+		message: 'Project Name:'
+	},
+	{
+		type: 'input',
+		name: 'repo',
+		message: 'Project Repo (/username/repo/):'
+	},
+	{
+		type: 'input',
+		name: 'readme',
+		message: 'Raw ReadMe URL (\'default\' for repo/master/README.md):'
+	},
+	{
+		type: 'input',
+		name: 'version',
+		message: 'Project Version:'
+	},
+	{
+		type: 'input',
+		name: 'description',
+		message: 'Project Description:'
+	},
+	{
+		type: 'input',
+		name: 'author',
+		message: 'Author\'s Name:'
+	},
+	{
+		type: 'input',
+		name: 'website',
+		message: 'Author\'s Website:'
+	}
+]
--- gulp/tasks/install.js
+++ gulp/tasks/install.js
@@ -1,72 +1,12 @@
 var	gulp   = require('gulp')
-,	yaml   = require('js-yaml')
-,	fs     = require('fs')
-,	prompt = require('gulp-prompt')
-// ,	rename = require('gulp-rename')
-,	_      = require('lodash')
+,	rename = require('gulp-rename')
+,	prompt = require('../pipes/config-prompt')
+
 
 module.exports = function() {
 	gulp.src('./_config.yml')
-		.pipe( prompt.prompt(questions, handler) )
+		// .pipe( rename("./_config.bak.yml") )
+		// .pipe( gulp.dest("./") )	
+		.pipe( prompt() )
 };
-
-var questions = [
-		{
-			type: 'input',
-			name: 'project',
-			message: 'Project Name:'
-		},
-		{
-			type: 'input',
-			name: 'repo',
-			message: 'Project Repo (URL):'
-		},
-		{
-			type: 'input',
-			name: 'readme',
-			message: 'Raw ReadMe URL (defaults to repo/master/README.md):'
-		},
-		{
-			type: 'input',
-			name: 'version',
-			message: 'Project Version:'
-		},
-		{
-			type: 'input',
-			name: 'description',
-			message: 'Project Description:'
-		},
-		{
-			type: 'input',
-			name: 'author',
-			message: 'Author\'s Name:'
-		},
-		{
-			type: 'input',
-			name: 'website',
-			message: 'Author\'s Website:'
-		}
-	]
-
-function handler( response) {
-	var config  = yaml.safeLoad( fs.readFileSync('./_config.yml', 'utf8') )
-	,	answers = getAnsObj( response )
-	,	answers = parseAnswers( answers )
-
-	 console.log( _.extend( config, answers ) )
-}	
-
-function getAnsObj( response ) {
-	var projectKeys = ['project', 'repo', 'readme', 'version', 'description']
-	,	authorKeys  = ['author', 'website']
-	,	answers     = {};
-
-	for (prop in response) {
-		var section       = _.indexOf(projectKeys, prop) != -1 ? "project" : "author"
-		,	 key          = prop === section ? "name" : prop;
-
-		answers[section]      = answers[section] || {}
-		answers[section][key] = response[prop]
-	}
-}
 
